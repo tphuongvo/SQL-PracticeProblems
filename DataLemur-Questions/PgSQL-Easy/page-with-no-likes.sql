6f8b7139f503f49e123c1d8bd3be7f240dbe5603
tapivo <86455398+tphuongvo@users.noreply.github.com>
2022-12-31 13:01:12 +0700

    add questions

---
+++ DataLemur-Questions/PgSQL-Easy/page-with-no-likes.sql
@@ -0,0 +1,43 @@
+/*
+Facebook
+URL: https://datalemur.com/questions/sql-page-with-no-likes
+Assume you are given the tables below about Facebook pages and page likes. 
+Write a query to return the page IDs of all the Facebook pages that don't have any likes. 
+The output should be in ascending order.
+
+pages Table:
+Column Name	    Type
+page_id	        integer
+page_name	    varchar
+
+pages Example Input:
+page_id	    page_name
+20001	    SQL Solutions
+20045	    Brain Exercises
+20701	    Tips for Data Analysts
+
+page_likes Table:
+Column Name	    Type
+user_id	        integer
+page_id	        integer
+liked_date	    datetime
+
+page_likes Example Input:
+user_id	    page_id	    liked_date
+111	        20001	    04/08/2022 00:00:00
+121	        20045	    03/12/2022 00:00:00
+156	        20001	    07/25/2022 00:00:00
+
+Example Output:
+page_id
+20701
+                **************************************************
+*/
+SELECT PAGE_ID 
+FROM PAGES
+WHERE 1=1
+  AND PAGE_ID NOT IN (
+      SELECT DISTINCT PAGE_ID FROM PAGE_LIKES 
+      )
+ORDER BY PAGE_ID ASC
+;
