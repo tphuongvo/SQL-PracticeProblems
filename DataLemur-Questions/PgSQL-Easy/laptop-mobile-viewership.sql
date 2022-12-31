3af8ab7870313143b708287c6329bbff3d5bf1e8
tapivo <86455398+tphuongvo@users.noreply.github.com>
2022-12-31 13:10:26 +0700

    add questions

---
+++ DataLemur-Questions/PgSQL-Easy/laptop-mobile-viewership.sql
@@ -0,0 +1,34 @@
+/*
+This is the same question as problem #3 in the SQL Chapter of Ace the Data Science Interview!
+
+Assume that you are given the table below containing information on viewership by device type (where the three types are laptop, tablet, and phone). Define “mobile” as the sum of tablet and phone viewership numbers. 
+Write a query to compare the viewership on laptops versus mobile devices.
+Output the total viewership for laptop and mobile devices in the format of "laptop_views" and "mobile_views".
+
+viewership Table:
+Column Name	    Type
+user_id	        integer
+device_type	    string ('laptop', 'tablet', 'phone')
+view_time	    timestamp
+
+viewership Example Input:
+user_id	    device_type	    view_time
+123	        tablet	        01/02/2022 00:00:00
+125	        laptop	        01/07/2022 00:00:00
+128	        laptop	        02/09/2022 00:00:00
+129	        phone	        02/09/2022 00:00:00
+145	        tablet	        02/24/2022 00:00:00
+
+Example Output:
+laptop_views	mobile_views
+2	3
+            **************************************************
+*/
+
+SELECT 
+  COUNT(CASE DEVICE_TYPE WHEN 'laptop' THEN 1 
+  ELSE NULL END) laptop_views,
+  COUNT(CASE WHEN device_type IN ('tablet','phone') THEN 1
+  ELSE NULL END) mobile_views
+FROM VIEWERSHIP
+;
