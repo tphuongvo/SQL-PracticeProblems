5ea16a58caac70344e54ff69972e705c9160ba50
tapivo <86455398+tphuongvo@users.noreply.github.com>
2023-02-18 16:42:34 +0700

    new source SQL

---
+++ LeetCode-Questions/SQL-I/183-customers-who-never-order.sql
@@ -0,0 +1,79 @@
+/*
+Table: Customers
+
++-------------+---------+
+| Column Name | Type    |
++-------------+---------+
+| id          | int     |
+| name        | varchar |
++-------------+---------+
+id is the primary key column for this table.
+Each row of this table indicates the ID and name of a customer.
+ 
+
+Table: Orders
+
++-------------+------+
+| Column Name | Type |
++-------------+------+
+| id          | int  |
+| customerId  | int  |
++-------------+------+
+id is the primary key column for this table.
+customerId is a foreign key of the ID from the Customers table.
+Each row of this table indicates the ID of an order and the ID of the customer who ordered it.
+ 
+
+Write an SQL query to report all customers who never order anything.
+
+Return the result table in any order.
+
+The query result format is in the following example.
+
+ 
+
+Example 1:
+
+Input: 
+Customers table:
++----+-------+
+| id | name  |
++----+-------+
+| 1  | Joe   |
+| 2  | Henry |
+| 3  | Sam   |
+| 4  | Max   |
++----+-------+
+Orders table:
++----+------------+
+| id | customerId |
++----+------------+
+| 1  | 3          |
+| 2  | 1          |
++----+------------+
+Output: 
++-----------+
+| Customers |
++-----------+
+| Henry     |
+| Max       |
++-----------+
+
+*/
+
+-- RUNTIME : 1029
+SELECT 
+    A.name AS Customers  
+FROM Customers A
+LEFT JOIN Orders B
+ ON A.id = B.customerId  
+WHERE B.customerId IS NULL
+    ;
+
+
+-- RUNTIME : 809
+SELECT 
+    name AS Customers
+FROM Customers
+ WHERE id NOT IN (SELECT customerid FROM Orders) 
+    ;
